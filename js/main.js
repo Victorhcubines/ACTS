93fd153afa5c209f956ce2f6f67951a8026a69fe
Victorhcubines <victorhcubines@gmail.com>
2025-08-06 16:56:11 -0300

    Delete js directory

--- js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("/api/denuncias")
-    .then(res => res.json())
-    .then(data => {
-      const container = document.getElementById("denuncias-container");
-      container.innerHTML = data.map(d => `<p>${d.titulo}</p>`).join("");
-    });
-});
